0e8ff4a383a88da57865505ee9774d15c639d9bc
PeriKILL0 <100075702+PeriKILL0@users.noreply.github.com>
2022-02-26 18:26:28 +0100

    # WARNING: head commit changed in the meantime
    Merge remote-tracking branch 'origin/master'

--- microcalculator/src/main/java/com/patriarch/calculator/CalculatorController.java
+++ microcalculator/src/main/java/com/patriarch/calculator/CalculatorController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/calculator")
 public class CalculatorController {
 
 	@RequestMapping("/sum")
