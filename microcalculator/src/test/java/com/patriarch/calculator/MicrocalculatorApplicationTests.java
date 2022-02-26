0c84ca1777fc2b91bd022b908d4f9d76eae2dc07
PeriKILL0 <100075702+PeriKILL0@users.noreply.github.com>
2022-02-26 13:08:04 +0100

    Starter Spring Boot project with Spring Web

---
+++ microcalculator/src/test/java/com/patriarch/calculator/MicrocalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patriarch.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
