f4ff0b353486755181f931612a1028864a85faaf
PeriKILL0 <100075702+PeriKILL0@users.noreply.github.com>
2022-02-27 00:27:18 +0100

    Tracing results.

--- microcalculator/src/main/java/com/patriarch/calculator/MicrocalculatorApplication.java
+++ microcalculator/src/main/java/com/patriarch/calculator/MicrocalculatorApplication.java
@@ -2,9 +2,34 @@ package com.patriarch.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.corp.calculator.TracerImpl;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(path = "/calculator")
 public class MicrocalculatorApplication {
+
+	TracerImpl tracer = new TracerImpl();
+
+	@RequestMapping("/sum")
+	public double sum(@RequestParam(name = "op1", defaultValue = "0") double op1,
+			@RequestParam(name = "op2", defaultValue = "0") double op2) {
+		double result = op1 + op2;
+		tracer.trace(result);
+		return result;
+	}
+
+	@RequestMapping("/sub")
+	public double sub(@RequestParam(name = "op1", defaultValue = "0") double op1,
+			@RequestParam(name = "op2", defaultValue = "0") double op2) {
+		double result = op1 - op2;
+		tracer.trace(result);
+		return result;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrocalculatorApplication.class, args);
--- microcalculator/src/test/java/com/patriarch/calculator/MicrocalculatorApplicationTests.java
+++ microcalculator/src/test/java/com/patriarch/calculator/MicrocalculatorApplicationTests.java
@@ -1,13 +1,28 @@
 package com.patriarch.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MicrocalculatorApplicationTests {
 
+	MicrocalculatorApplication calculator = new MicrocalculatorApplication();
+
 	@Test
-	void contextLoads() {
+	void testSum() {
+		double result = calculator.sum(4.3, 5.5);
+		
+		assertEquals(9.8, result, 0.0);
+	}
+
+	@Test
+	void testSub() {
+		double result = calculator.sub(10.4, 3.5);
+		
+		assertEquals(6.9, result, 0.0);
+		
 	}
 
 }
