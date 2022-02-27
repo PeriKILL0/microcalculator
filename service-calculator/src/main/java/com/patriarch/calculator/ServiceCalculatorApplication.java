f6ef774b53202b861b3844a6977f4a9efc2ba402
PeriKILL0 <100075702+PeriKILL0@users.noreply.github.com>
2022-02-27 01:29:54 +0100

    Project microservice

---
+++ service-calculator/src/main/java/com/patriarch/calculator/ServiceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.patriarch.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCalculatorApplication.class, args);
+	}
+
+}
